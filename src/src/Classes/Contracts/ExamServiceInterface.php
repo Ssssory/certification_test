fd744ef61ab49fa262ebb25bba3bef442fe2a01b
ssssory <itrubchaninov@list.ru>
2023-09-27 15:03:36 +0300

    add methods to contract

--- src/src/Classes/Contracts/ExamServiceInterface.php
+++ src/src/Classes/Contracts/ExamServiceInterface.php
@@ -7,8 +7,10 @@ use App\Entity\Exam;
 
 interface ExamServiceInterface
 {
+    function getExamById(int $id): Exam;
     function startExam(string $name): Exam;
     function finishExam(Exam $exam);
     function addAnswers(AnswerDto $dto);
     function getAllCompleteExams(): array;
+    function prepareResult(int $examId): array;
 }
